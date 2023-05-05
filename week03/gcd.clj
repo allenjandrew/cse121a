b29b7f5a6b0b281b9fdeb882ec429b9465a2fae6
allenjandrew <allenjandrew@gmail.com>
2023-05-04 22:02:27 -0600

    changed recursive use of gcd to recur

--- week03/gcd.clj
+++ week03/gcd.clj
@@ -1,7 +1,7 @@
 (defn gcd [num1 num2] ;; takes two parameters
   (if (= 0 (rem num1 num2))
     num2 ;; if num1 % num2 == 0, return num2
-    (gcd num2 (rem num1 num2)))) ;; else find gcd of num2 and (num1 % num2)
+    (recur num2 (rem num1 num2)))) ;; else find gcd of num2 and (num1 % num2)
 
 (gcd 480 1001) ;; finds the greatest common denominator of 480 and 1001 (1)
 
